2cc0e17b1a01bca10b726805ec15ae48433cea0a
Thomas McKanna <thomas@Thomass-Air.attlocal.net>
2017-02-20 17:25:29 -0600

    initial commit

---
+++ FunnyHeadlines/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  FunnyHeadlines
+//
+//  Created by Thomas McKanna on 2/20/17.
+//  Copyright © 2017 ISYS 220. All rights reserved.
+//
+
+import Foundation
+
+// This file will use the NewsAPI (https://newsapi.org/)
+
+// TODO: this file will define the functions needed to return an array of headlines.
